1ead817fdd228b11ced04bf075c959e58f5da2cd
melwintjoshy <melwin465@gmail.com>
2025-08-09 18:42:38 +0530

    updated

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,15 +1,9 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/**',
-      },
-    ],
+  eslint: {
+    // Warning: This allows production builds to successfully complete
+    // even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
